054b7ff478fc0ba59a7298d6efefc1a44d05ba76
DanyaChan <danil.droid2000@gmail.com>
2022-09-08 16:24:31 +0400

    fix bug refactor

--- index.js
+++ index.js
@@ -6,83 +6,109 @@ const start_x = 100;
 
 const margin = 2;
 const size = 200;
-const color = '#a38e8e';
 const empty_color = '#cfcfcf';
 
 const r_start = 200;
 const g_start = 180;
 const b_start = 150;
 
+function getEmptyArray (x, y) {
+    const res = new Array(y);
+    for (const i of res.keys()) {
+        res[i] = new Array(x);
+        for (const j of res[i].keys()) {
+            res[i][j] = null;
+        }
+    }
+    return res;
+}
 
-function calcColor(n) {
+function calcColor (n) {
     const l = Math.log2(n);
     const r = r_start;
     const g = Math.floor(g_start / Math.max(1, 0.3 * l));
     const b = Math.floor(b_start / l);
 
-    return '#' + r.toString(16) + g.toString(16) + b.toString(16);
+    return '#' + r.toString(16).padStart(2, '0') + g.toString(16).padStart(2, '0') + b.toString(16).padStart(2, '0');
 }
 
+function getId (x, y) {
+    return '(' + x + ',' + y + ')';
+}
 
-function getHTMLTile(x, y, n) {
-    var div = document.createElement('div');
+function getHTMLTile (x, y, n) {
+    let div = document.getElementById(getId(x, y));
+    if (div === null) {
+        div = document.createElement('div');
+        div.id = getId(x, y);
+    }
     div.style.top = getPx(y);
     div.style.left = getPx(x);
     div.style.height = getPx(size);
     div.style.width = getPx(size);
     div.style.position = 'absolute';
+    div.className = 'tile';
 
-    var p = document.createElement('p');
+    const ps = div.getElementsByTagName('p');
+    let p;
+    if (ps.length === 0) {
+        p = document.createElement('p');
+        div.appendChild(p);
+    } else {
+        p = ps[0];
+    }
 
-    p.style.fontSize = '40pt'
-    p.style.textAlign = 'center'  
+    p.style.fontSize = '40pt';
+    p.style.textAlign = 'center';
     p.style.position = 'relative';
-    p.style.fontFamily = 'sans-serif'
+    p.style.fontFamily = 'sans-serif';
     p.style.top = '6%';
-
 
     if (n === null) {
         div.style.backgroundColor = empty_color;
+        p.innerHTML = '';
     } else {
         div.style.backgroundColor = calcColor(n);
         p.innerHTML = '' + n;
-        div.appendChild(p);
     }
-
-    
 
     return div;
 }
 
-function getPx(n) {
+function clearTiles () {
+    const divs = document.getElementsByClassName('tile');
+    for (const e of divs) {
+        e.remove();
+    }
+}
+
+function getPx (n) {
     return n + 'px';
 }
 
-function renderTiles(array) {
-    for (let y in array) {
-        for (let x in array[y]) {
-
+function renderTiles (array) {
+    for (const y in array) {
+        for (const x in array[y]) {
             const x_ = start_x + x * (margin + size);
             const y_ = start_y + y * (margin + size);
 
-            let d = getHTMLTile(x_, y_, array[y][x]);
+            const d = getHTMLTile(x_, y_, array[y][x]);
             document.body.insertBefore(d, null);
         }
     }
 }
 
-function getRandomInt(max) {
+function getRandomInt (max) {
     return Math.floor(Math.random() * max);
 }
 
-
-function insertInRandom(tiles) {
+function insertInRandom (tiles) {
     const free = getFreeSpace(tiles);
     let pos = getRandomInt(free);
-    let value = getRandomInt(4);
+    const value = getRandomInt(4);
 
-    for (let y in tiles) {
-        for (let x in tiles[y]) {
+    for (const y in tiles) {
+        for (const x in tiles[y]) {
             if (tiles[y][x] === null && (pos--) == 0) {
                 tiles[y][x] = value === 3 ? 4 : 2;
                 break;
@@ -91,20 +117,19 @@ function insertInRandom(tiles) {
     }
 }
 
-function leftTurn(tiles) {
-    for (let i of tiles.keys()) {
+function leftTurn (tiles) {
+    for (const i of tiles.keys()) {
         tiles[i] = leftTurnLine(tiles[i]);
     }
 }
 
-
-function rightTurn(tiles) {
-    for (let i of tiles.keys()) {
+function rightTurn (tiles) {
+    for (const i of tiles.keys()) {
         tiles[i] = rightTurnLine(tiles[i]);
     }
 }
 
-function upTurn(tiles) {
+function upTurn (tiles) {
     const ysize = tiles.length;
     const xsize = tiles[0].length;
 
@@ -122,7 +147,7 @@ function upTurn(tiles) {
     }
 }
 
-function downTurn(tiles) {
+function downTurn (tiles) {
     const ysize = tiles.length;
     const xsize = tiles[0].length;
 
@@ -142,11 +167,11 @@ function downTurn(tiles) {
 
 /**
  * Not my proudest moment but it was more testing js in browser than actually writing alghorithm
- * @param {Array} line 
+ * @param {Array} line
  */
-function leftTurnLine(line) {
+function leftTurnLine (line) {
     let res = [];
-    for (let i of line.keys()) {
+    for (const i of line.keys()) {
         if (line[i] === null) {
             continue;
         }
@@ -161,35 +186,33 @@ function leftTurnLine(line) {
 }
 
 /**
- * 
- * @param {Array} line 
+ *
+ * @param {Array} line
  */
-function rightTurnLine(line) {
+function rightTurnLine (line) {
     let res = [];
-    for (let i of line.keys()) {
+    for (const i of line.keys()) {
         if (line[i] === null) {
             continue;
         }
         res.push(line[i]);
     }
     res = merge(res);
-    let r = [];
+    const r = [];
     while (r.length + res.length != line.length) {
         r.push(null);
     }
 
-    for (let e of res) {
+    for (const e of res) {
         r.push(e);
     }
 
     return r;
 }
 
-
-function merge(line) {
-    let r = [];
+function merge (line) {
+    const r = [];
     for (let i = 0; i < line.length; i++) {
-
         if (i == line.length - 1) {
             r.push(line[i]);
             break;
@@ -205,11 +228,10 @@ function merge(line) {
     return r;
 }
 
-
-function getFreeSpace(tiles) {
+function getFreeSpace (tiles) {
     let cnt = 0;
-    for (let y in tiles) {
-        for (let x in tiles[y]) {
+    for (const y in tiles) {
+        for (const x in tiles[y]) {
             if (tiles[y][x] === null) {
                 cnt++;
             }
@@ -218,53 +240,46 @@ function getFreeSpace(tiles) {
     return cnt;
 }
 
-function main() {
+function getFieldSize () {
+    const str = prompt('Insert field size (like "4 4")', '4 4');
+    const [x, y] = str.split(' ');
+    return {
+        x: Number(x),
+        y: Number(y)
+    };
+}
 
-    let tiles = [
-        [null, null, null, null],
-        [null, null, null, null],
-        [null, null, null, null],
-        [null, null, null, null],
-    ];
+function main () {
+    let sz = getFieldSize();
 
-    const arrowKeys = ['ArrowUp', 'ArrowDown', 'ArrowLeft', 'ArrowRight', 'm'];
+    let tiles = getEmptyArray(sz.x, sz.y);
+
+    const allowed_keys = ['ArrowUp', 'ArrowDown', 'ArrowLeft', 'ArrowRight', 'm'];
 
     renderTiles(tiles);
 
     document.addEventListener('keydown', function (event) {
-        if (arrowKeys.includes(event.key)) {
-
-
+        if (allowed_keys.includes(event.key)) {
             if (event.key === 'ArrowUp') {
                 upTurn(tiles);
-
-            }
-            else if (event.key === 'ArrowDown') {
+            } else if (event.key === 'ArrowDown') {
                 downTurn(tiles);
-
-            }
-            else if (event.key === 'ArrowLeft') {
+            } else if (event.key === 'ArrowLeft') {
                 leftTurn(tiles);
-
-            }
-            else if (event.key === 'ArrowRight') {
+            } else if (event.key === 'ArrowRight') {
                 rightTurn(tiles);
-
             }
 
             if (getFreeSpace(tiles) === 0) {
-                alert('Is this loss');
-                tiles = [
-                    [null, null, null, null],
-                    [null, null, null, null],
-                    [null, null, null, null],
-                    [null, null, null, null],
-                ];
+                alert('You lost');
+                sz = getFieldSize();
+                console.log(sz);
+                tiles = getEmptyArray(sz.x, sz.y);
             }
 
             insertInRandom(tiles);
             renderTiles(tiles);
+            clearTiles();
         }
     });
 }
-
